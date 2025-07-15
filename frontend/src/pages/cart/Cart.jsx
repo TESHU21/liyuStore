c6ac7006576c0886ceba9434b7f535e52c869b66
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-07-15 20:53:14 +0300

    fix cart responsiveness

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -13,14 +13,14 @@ const Cart = () => {
   return (
     <div className=' pb-20'>
       <PageHeader header={headers}/>
-      <div className='md:pt-10'>
+      <div className=' pt-4 px-4 md:pt-10'>
         {cart.length>0?( <div>
            {cart.map((item)=><CartItem key={item._id} cart={item}/>)}
            <div className='max-w-4xl mx-auto'>
               <div className="text-lg font-medium mb-2">Items : {totalQuantity}</div>
       <div className="text-xl font-semibold mb-6">Total : ${totalPrice.toLocaleString()}</div>
 
-      <button className="bg-blue-primary w-1/2 text-white px-6 py-3 rounded-md hover:bg-blue-primary mx-auto"
+      <button className="bg-blue-primary md:w-1/2 text-white px-6 py-3 rounded-md hover:bg-blue-primary mx-auto"
       onClick={()=>navigate("/checkout")}
       >
         Proceed to checkout
