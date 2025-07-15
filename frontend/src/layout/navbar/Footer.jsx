bbcaff10ecebbfa062af7851215ab89e9e9c35bd
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-07-15 20:02:41 +0300

    fix resposnivess footer

--- frontend/src/layout/navbar/Footer.jsx
+++ frontend/src/layout/navbar/Footer.jsx
@@ -4,14 +4,15 @@ import MasterCardIcon from "../../assets/MastercardIcon.svg"
 import VisaIcon from "../../assets/VisaIcon.svg"
 const Footer = () => {
   return (
-    <div className=' flex  justify-between items-center px-[40px] pt-[42px] pb-[21px] border-t'>
+    <div className=' flex flex-col md:flex-row justify-between items-center px-[40px] pt-[42px] pb-[21px] border-t'>
       <div className=' flex items-center gap-4 '>
         <img src={VisaIcon} alt=""  className=' w-[65px] h-[65px] '/>
         <img src={PaypalIcon} alt="" className=' w-[64px] h-[64px] ' />
         <img src={MasterCardIcon} alt="" className=' w-[64.5px] h-[43px]' />
 
       </div>
-      <p>2022 Evershop. All Rights Reserved.</p>
+
+      © 2022 Evershop.All rights reserved.
     </div>
   )
 }
