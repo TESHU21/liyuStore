7796b05050aa6d1d74e9bcc09adb2f1447788b91
Teshome Mosneh <teshome.mosneh@gmail.com>
2025-07-22 21:34:19 +0300

    fix cors issues

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "'https://liyumart.netlify.app'"  // 
+  "https://liyumart.netlify.app"  // 
+
 ];
 
 app.use(
