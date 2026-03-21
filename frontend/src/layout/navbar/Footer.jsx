bb457e41e735d3e4f1d08cbc02dd23102408f40a
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-03-21 21:55:32 +0300

    apply performance improvements

--- frontend/src/layout/navbar/Footer.jsx
+++ frontend/src/layout/navbar/Footer.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import PaypalIcon from "../../assets/paypalIcon.svg"
-import MasterCardIcon from "../../assets/MastercardIcon.svg"
-import VisaIcon from "../../assets/VisaIcon.svg"
+import React from "react";
+import PaypalIcon from "../../assets/paypalIcon.svg";
+import MasterCardIcon from "../../assets/MastercardIcon.svg";
+import VisaIcon from "../../assets/VisaIcon.svg";
 const Footer = () => {
   return (
-    <div className=' flex flex-col md:flex-row justify-between items-center px-[40px] pt-[42px] pb-[21px] border-t'>
-      <div className=' flex items-center gap-4 '>
-        <img src={VisaIcon} alt=""  className=' w-[65px] h-[65px] '/>
-        <img src={PaypalIcon} alt="" className=' w-[64px] h-[64px] ' />
-        <img src={MasterCardIcon} alt="" className=' w-[64.5px] h-[43px]' />
-
+    <div className=" flex flex-col md:flex-row justify-between items-center px-[40px] pt-[42px] pb-[21px] border-t">
+      <div className=" flex items-center gap-4 ">
+        <img
+          src={VisaIcon}
+          loading="lazy"
+          alt=""
+          className=" w-[65px] h-[65px] "
+        />
+        <img
+          src={PaypalIcon}
+          loading="lazy"
+          alt=""
+          className=" w-[64px] h-[64px] "
+        />
+        <img
+          src={MasterCardIcon}
+          loading="lazy"
+          alt=""
+          className=" w-[64.5px] h-[43px]"
+        />
       </div>
-
       © 2025 LiyuStore.All rights reserved.
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,44 +1,71 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Layout from '@/layout/Layout'
-import Home from '@/pages/home/Home'
-import Shop from '@/pages/shop/Shop'
-import ShopDetail from '@/pages/shop/user/detail/ShopDetail'
-import Cart from '@/pages/cart/Cart'
-import Favourite from '@/pages/favourite/Favourite'
-import Login from '@/pages/auth/login/Login'
-import Checkout from '@/pages/checkout/Checkout'
-import CreateProduct from '@/pages/shop/admin/ProductFormPage'
-import Orders from '@/pages/orders/Orders'
-import Profile from '@/pages/profile/Profile'
-import CategoryManager from "../pages/catagory/CategoryManager"
-import OrdersDetail from '@/pages/orders/components/OrdersDetail'
-import UpdateUserProfile from '@/pages/profile/user/UpdateUserProfile'
-import User from '@/pages/users/User'
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+// 🔥 Lazy imports
+const Layout = React.lazy(() => import("@/layout/Layout"));
+const Home = React.lazy(() => import("@/pages/home/Home"));
+const Shop = React.lazy(() => import("@/pages/shop/Shop"));
+const ShopDetail = React.lazy(
+  () => import("@/pages/shop/user/detail/ShopDetail"),
+);
+const Cart = React.lazy(() => import("@/pages/cart/Cart"));
+const Favourite = React.lazy(() => import("@/pages/favourite/Favourite"));
+const Login = React.lazy(() => import("@/pages/auth/login/Login"));
+const Checkout = React.lazy(() => import("@/pages/checkout/Checkout"));
+const CreateProduct = React.lazy(
+  () => import("@/pages/shop/admin/ProductFormPage"),
+);
+const Orders = React.lazy(() => import("@/pages/orders/Orders"));
+const Profile = React.lazy(() => import("@/pages/profile/Profile"));
+const CategoryManager = React.lazy(
+  () => import("@/pages/catagory/CategoryManager"),
+);
+const OrdersDetail = React.lazy(
+  () => import("@/pages/orders/components/OrdersDetail"),
+);
+const UpdateUserProfile = React.lazy(
+  () => import("@/pages/profile/user/UpdateUserProfile"),
+);
+const User = React.lazy(() => import("@/pages/users/User"));
+const PerformanceDashboard = React.lazy(
+  () => import("@/components/PerformanceDashboard"),
+);
+
 const AppRoutes = () => {
   return (
-    <Routes >
-        <Route path='/' element={<Layout/>}>
-            <Route index element={<Home/>}/>
-        <Route path='shop' element={<Shop/>}/>
-        <Route path='shop/:id' element={<ShopDetail/>}/>
-        <Route path='cart' element={<Cart/>}/>
-        <Route path ="favourite" element={<Favourite/>}/>
-        <Route path='detail' element={<ShopDetail/>}/>
-        <Route path="checkout" element={<Checkout/>}/>
-        <Route path="product" element={<CreateProduct/>}/>
-        <Route path="profile" element={<Profile/>}/>
-        <Route path="category" element={<CategoryManager/>}/>
-        <Route path="orders" element={<Orders/>}/>
-        <Route path='orders/:id' element={<OrdersDetail/>}/>
-        <Route path='update-profile' element={<UpdateUserProfile/>}/>
-        <Route path='/user' element={<User/>}/>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          Loading...
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="shop/:id" element={<ShopDetail />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="favourite" element={<Favourite />} />
+          <Route path="detail" element={<ShopDetail />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="product" element={<CreateProduct />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="category" element={<CategoryManager />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="orders/:id" element={<OrdersDetail />} />
+          <Route path="update-profile" element={<UpdateUserProfile />} />
 
+          {/* ✅ fixed path */}
+          <Route path="user" element={<User />} />
+
+          <Route path="performance" element={<PerformanceDashboard />} />
         </Route>
-        <Route path='login' element={<Login/>}/>
-        
-    </Routes>
-  )
-}
 
-export default AppRoutes
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default AppRoutes;
