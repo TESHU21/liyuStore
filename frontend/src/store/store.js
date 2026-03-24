f0f8e2f47a5495dc467da7f6918db0d3f10735e8
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-03-24 10:05:31 +0300

    configure   RTK Query to app and main

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,27 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from "./authSlice"
-import uiReducer from "./uiSlice"
-import categoryReducer from "./categorySlice"
-import productReducer from "./productSlice"
-import selectedProductReducer from "./selectedProductSlice"
-import cartReducer from "./cartSlice"
-import favouriteReducer from "./favoriteSlice"
-import Favourite from '@/pages/favourite/Favourite';
-import userReducer from "./userSlice"
-import orderReducer from "./orderSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
+import uiReducer from "./uiSlice";
+import categoryReducer from "./categorySlice";
+import productReducer from "./productSlice";
+import selectedProductReducer from "./selectedProductSlice";
+import cartReducer from "./cartSlice";
+import favouriteReducer from "./favoriteSlice";
+import Favourite from "@/pages/favourite/Favourite";
+import userReducer from "./userSlice";
+import orderReducer from "./orderSlice";
+import { baseApi } from "./api/baseApi";
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    ui:uiReducer,
-    category:categoryReducer,
-    products:productReducer,
-    selectedProduct:selectedProductReducer,
-    cart:cartReducer,
-    favourite:favouriteReducer,
-    users:userReducer,
-    orders:orderReducer,
+    ui: uiReducer,
+    category: categoryReducer,
+    products: productReducer,
+    selectedProduct: selectedProductReducer,
+    cart: cartReducer,
+    favourite: favouriteReducer,
+    users: userReducer,
+    orders: orderReducer,
     // add other slices here (e.g., product, cart, order)
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export default store;
