d324e9dc39511f4111c11b2cbfbde8ecb3fc7fd8
Teshome Mosneh <teshome.mosneh@gmail.com>
2026-03-25 11:18:33 +0300

    add authApi.js

---
+++ frontend/src/store/api/authApi.js
@@ -0,0 +1,43 @@
+import { baseApi } from "@/store/api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    loginUser: builder.mutation({
+      query: (credentials) => ({
+        url: "/api/users/auth",
+        method: "POST",
+        data: credentials,
+      }),
+    }),
+    registerUser: builder.mutation({
+      query: (userData) => ({
+        url: "/api/users",
+        method: "POST",
+        data: userData,
+      }),
+    }),
+    getCurrentUserProfile: builder.query({
+      query: () => ({
+        url: "/api/users/profile",
+        method: "GET",
+      }),
+      providesTags: [{ type: "Users", id: "ME" }],
+      keepUnusedDataFor: 300,
+    }),
+    updateProfile: builder.mutation({
+      query: (userData) => ({
+        url: "/api/users/profile",
+        method: "PUT",
+        data: userData,
+      }),
+      invalidatesTags: [{ type: "Users", id: "ME" }],
+    }),
+  }),
+});
+
+export const {
+  useLoginUserMutation,
+  useRegisterUserMutation,
+  useGetCurrentUserProfileQuery,
+  useUpdateProfileMutation,
+} = authApi;
